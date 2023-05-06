394d3bfccc33a454b3f988363d15b99861268e3a
KTrzyna <79695304+KTrzyna@users.noreply.github.com>
2023-05-06 20:00:39 +0200

    .

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Hello World!</h1>
+        <h1>Hello!</h1>
         <TextField />
       </header>
     </div>
